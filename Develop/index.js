f5db84a5fd992a10c1e1d6c60dfb8a0664ae56c5
NickSchenck <yoyo4games@yahoo.com>
2022-06-27 13:37:30 -0500

    Changed questions

--- Develop/index.js
+++ Develop/index.js
@@ -3,59 +3,45 @@ const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = () => {
     return inquirer.prompt([
-        {
-            type: "input",
-            name: "title",
-            message: "What is your projects name? (Required)",
-            validate: titleInput => {
-                if (titleInput) {
-                    return true;
-                } else {
-                    console.log("Please enter your projects name.");
-                    return false;
-                }
-            }
+        {type: 'input',
+        message: 'What is the title of the project?',
+        name: 'title'},
+
+        {type: 'input',
+        message: 'Describe your project:',
+        name: 'description'
         },
-        {
-            type: "input",
-            name: "descript",
-            message: "Describe your project. (Required)",
-            validate: descriptInput => {
-                if (descriptInput) {
-                    return true;
-                } else {
-                    console.log("Please enter a desciption for your project.");
-                    return false;
-                }
-            }
+
+        {type: 'input',
+        message: 'How do you install your project?',
+        name: 'install'
         },
-        {
-            type: "input",
-            name: "install",
-            message: "How do you install your project? (Required)",
-            validate: installInput => {
-                if (installInput) {
-                    return true;
-                } else {
-                    console.log("Please detail how a user would install your project.");
-                    return false;
-                }
-            }
+
+        {type: 'input',
+        message: 'How is your project used?',
+        name: 'usage'
         },
-        {
-            type: "input",
-            name: "use",
-            message: "How/what would a user implement your project for? (Required)",
-            validate: useInput => {
-                if (useInput) {
-                    return true;
-                } else {
-                    console.log("Please describe why a user would want your project.");
-                    return false;
-                }
-            }
+
+        {type: 'input',
+        message: 'What tests run with your project?',
+        name: 'tests'
+        },
+
+        {type: 'input',
+        message: 'How can people contribute to your project?',
+        name: 'contribute'
+        },
+
+        {type: 'input',
+        message: 'What is your GitHub user name?',
+        name: 'github'
+        },
+
+        {type: 'input',
+        message: 'What is your email address?',
+        name: 'email'
         }
-    ]); 
+    ]);
 };
 
 // TODO: Create a function to write README file
